b1ea28adf0189b12a57c4d1e2b2b7ca0cbf66ab5
Oscar Dowson <o.dowson@gmail.com>
2026-06-15 13:43:06 +1200

    Add license headers

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,9 @@
+# Copyright (c) 2026 Martin Kirk Bonde, James Daniel Foster and contributors
+# Copyright (c) 2020-2023 GAMS Software GmbH
+#
+# Use of this source code is governed by an MIT-style license that can be found
+# in the LICENSE.md file or at https://opensource.org/licenses/MIT.
+
 import Test: @testset, @test, @test_throws
 import Tables
 import DataFrames: DataFrame, names, metadata!
